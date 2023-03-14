fb1b1ebe11a7d3189519e8b9f8f5b55a6f4f899d
Jonathana1106 <jonathana1196@gmail.com>
2023-03-14 01:10:01 -0600

    [EDIT] comments

--- keyhandler.asm
+++ keyhandler.asm
@@ -9,45 +9,36 @@ enable_keyhandler:
   mov [gs:bx], word keyhandler
   mov [gs:bx + 2], ds ; segment
   sti ; reenable interrupts
-
   ret
 
 keyhandler:
   in al, 0x60 ; get key data from port 0x60
   mov bl, al ; save it on bl
   mov byte [port60], al
-  
   in al, 0x61
   mov ah, al
   or al, 0x80 ; disable bit 7
   out 0x61, al ; send it back
   xchg ah, al ; get original
   out 0x61, al ; send it back
-
   mov al, 0x20 ; end of interrupt
   out 0x20, al
-
   and bl, 0x80 ; key release
   jnz idone ; dont repeat
-
   ; --- Return 
   mov ax, [port60]
-
   ; -- When on the start screen it should only respond to space presses
   cmp byte [current_level], 0
   je waiting_for_enter
-  
   ; -- When finishing the game, it should only respond to R
   cmp byte [current_level], 3
   je waiting_for_reset
-
   jmp on_game_key_handler
 
 waiting_for_enter:
   cmp ax, 0x001c ; Enter key
   je enter_pressed
   jne switch_keys_done
-  
 
 enter_pressed:
   mov byte [current_level], 0x01
@@ -57,21 +48,16 @@ on_game_key_handler:
   ; -- Checking for presses of the L key regardless of pause status
   cmp ax, 0x0026
   je L_key_pressed
-
   ; -- When paused it should only respond to L presses
   cmp byte [pause_status], 1
   je switch_keys_done
-
   ; -- Checking the rest of the keys
   cmp ax, 0x0050
   je down_key_pressed
-
   cmp ax, 0x004B
   je left_key_pressed
-
   cmp ax, 0x0048
   je up_key_pressed
-
   cmp ax, 0x004D
   je right_key_pressed
 
@@ -82,6 +68,7 @@ waiting_for_reset:
   
 switch_keys_done_refresh:
   call refresh_screen
+
 switch_keys_done:
   jmp idone
 
@@ -133,7 +120,6 @@ L_key_pressed:
   mov si, pause_msg
   call print
   call toggle_pause_status
-
   cmp byte [pause_status], 1
   je start_pausing
   jne start_unpausing
