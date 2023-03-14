bd832f9e7aa90f84b6ecd0da7aa4a88ee968140d
Jonathana1106 <jonathana1196@gmail.com>
2023-03-14 01:22:52 -0600

    [EDIT] comments & format

--- screen_info.asm
+++ screen_info.asm
@@ -2,19 +2,14 @@ print_info:
   call move_video_cursor_to_0
   call obstacles_overcome_count
   call print_newline
-
   call level_msg
   call print_newline
-
   call print_game_name
   call print_newline
-
   call print_game_controls
-
   cmp byte [current_level], 3
   jne done
   call print_finish_game_msg
-
   jmp done
 
 print_finish_game_msg:
@@ -31,30 +26,26 @@ print_game_controls:
   call move_video_cursor_col
   mov si, move_controls_msg
   call print
-
   mov byte [row_video_cursor_to], 1
   call move_video_cursor_row
-
   mov si, pause_controls_msg
   call print
-
   mov byte [row_video_cursor_to], 2
   call move_video_cursor_row
-
   mov si, reset_controls_msg
   call print
-
   jmp done
 
 obstacles_overcome_count:
   mov al, byte [current_level]
   cmp al, 1
   jne obstacles_lv2
-
   mov si, obstacles_lv1_msg
   jmp obstacles_print
+
 obstacles_lv2:
   mov si, obstacles_lv2_msg
+
 obstacles_print:
   call print
   jmp done
@@ -65,8 +56,10 @@ level_msg:
   jne level2_msg
   mov si, level_1_msg
   jmp level_print
+
 level2_msg:
   mov si, level_2_msg
+
 level_print:
     call print
     jmp done
@@ -75,4 +68,3 @@ print_game_name:
   mov si, game_name_str
   call print
   jmp done
-  
